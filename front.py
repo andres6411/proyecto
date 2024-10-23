bdaff3c505ddaa066af262a4d462f15e6b17c97c
andres ortega <pes6411@gmail.com>
2024-10-22 22:01:08 -0500

    push

--- front.py
+++ front.py
@@ -4,27 +4,27 @@ st.title(" Asistente virtual")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
-    if "first message" not in st.session_state:
-        st.session_state.first_message = True
+if "first_message" not in st.session_state:
+    st.session_state.first_message = True
 
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-                if st.session_state.first_message:
-                    with st.chat_message("assistant"):
-                        st.markdown("Hola, ¿cómo puedo ayudarte?")
+if st.session_state.first_message:
+    with st.chat_message("assistant"):
+        st.markdown("Hola, ¿cómo puedo ayudarte?")
 
-                        st.session_state.message.append({"role": "assistant", "content": "Hola, ¿como puedo ayudarte?"})
-                        st.session_state.first_message = False  
+    st.session_state.messages.append({"role": "assistant", "content": "Hola, ¿cómo puedo ayudarte?"})
+    st.session_state.first_message = False  
 
 
-                        if prompt := st.chat_input("¿como puedo ayudarte?"):
-                            with st.chat_message("user"):
-                                st.markdown(prompt)
-                                st.session_state.messages.append({"role": "user", "content": prompt})
+if prompt := st.chat_input("¿cómo puedo ayudarte?"):
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-                                with st.chat_message("assistent"):
-                                    st.markdown(prompt)
+    with st.chat_message("assistent"):
+        st.markdown(prompt)
 
-                                    st. session_state.messages.append({"role": "assistent", "content": prompt})
+    st. session_state.messages.append({"role": "assistent", "content": prompt})
